8dd02e476ed303c3420f294e872d4ca3455cab88
Jason Livesay <ithkuil@gmail.com>
2015-04-26 16:42:56 +0000

    update

--- www/js/index.js
+++ www/js/index.js
@@ -44,11 +44,17 @@ var app = {
     },
 
     onGo: function() {
+        var frm = document.getElementById('frm');
+        var proj = document.getElementById('run').value;
+        frm.src = 'https://raw.githubusercontent.com/'+proj+'/master/index.html?zz='+(new Date()).getTime();
+        
+        /*
         var proj = document.getElementById('run').value;
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.src = 'https://raw.githubusercontent.com/'+proj+'/master/index.js?zz='+(new Date()).getTime();
         document.getElementsByTagName('head')[0].appendChild(script);
+        */
     },//
     onReset: function() {
         location.reload();
